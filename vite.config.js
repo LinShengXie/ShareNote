609f0750af8b9c232ea48faf65e71716787abdbc
yijia.xie <2855125067@qq.com>
2025-02-14 15:49:09 +0800

    feat:init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ShareNote/',  // Add this line - should match your repository name
   plugins: [vue()],
-  base: './'
 })
